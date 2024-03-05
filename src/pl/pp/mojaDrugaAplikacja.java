519c0661e53276ce79af58bed35bd05c24597ba4
DanyloSolomatin <s.daniil14122005@gmail.com>
2024-03-05 13:23:04 +0100

    Pierwszy zapis do repozytorium Lab2

---
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -0,0 +1,18 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class mojaDrugaAplikacja {
+    public static void main(String[] args) {
+        int x = 10;
+
+        int wynik = x * 2;
+
+        int xSquared = x * x;
+
+
+        System.out.println("Wartość x: " + x);
+        System.out.println("Dwa razy Wartość x:" + wynik);
+        System.out.println("Kwadrat x: " + xSquared);
+    }
+}
